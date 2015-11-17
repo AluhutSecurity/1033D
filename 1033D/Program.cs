a11091eda747d3e90e2e387d7653e357f8dcb07f
Snaep <kSnaep@web.de>
2015-11-17 22:51:12 +0100

    simple example, connect, login and send random coords

--- 1033D/Program.cs
+++ 1033D/Program.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace _1033D {
     class Program {
+        static uint MYUID = 22347;
+        static ushort sequenceCount;
+
+        private static void Handler() {
+
+        }
+
         static void Main( string[] args ) {
+
+            var client = BlakcMakiga.Abrakadubra.CreateInstance( BlakcMakiga.BlakcInstanceIdentifier.BlakcClient ) as BlakcMakiga.Net.IBlakcClient;
+            client.Connect( new System.Net.IPEndPoint( _1033C.Defines.DEFAULT_DRONESERVER_IP, _1033C.Defines.DEFAULT_DRONESERVER_LOCALPORT ) );
+
+            bool loginresult = client.Login( MYUID );
+            if ( loginresult == false ) {
+                Console.WriteLine( "login rejected" );
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine( "login successful" );
+            sequenceCount = 0;
+
+            var packageGenerator = new _1033C.Drones.MyDronePacketGenerator( BlakcMakiga.Utils.CRC.CRCMode.CRC16 );
+            _1033C.Drones.MyDronePacket packet;
+            Random r = new Random();
+
+            while ( true ) {
+                System.Threading.Thread.Sleep( 1000 );
+                byte[] blob = BitConverter.GetBytes( r.NextDouble() * 100 ).Concat( BitConverter.GetBytes( r.NextDouble() * 100 ) ).Concat( BitConverter.GetBytes( r.NextDouble() * 20 ) ).ToArray();
+                packet = packageGenerator.GeneratePacket( _1033C.Drones.MyDronePacketContent.LocationUpdate, blob );
+                client.Send( sequenceCount++, packet.Serialize() );
+            }
+
+            packet = packageGenerator.GeneratePacket( _1033C.Drones.MyDronePacketContent.SignalBye );
+            client.Send( ++sequenceCount, packet.Serialize() );
+
+            client.Stop();
+            Console.WriteLine( "disconnected" );
+            Console.ReadKey();
         }
     }
 }
